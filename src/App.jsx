21db81fa3981cf3d936787f7fbc2cb60206ddecc
SuperCoderSuat <supercodesuat@gmail.com>
2023-12-11 16:21:49 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-import TodoList from './components/todolist/ToDoList'
+import ToDoList from './components/todolist/ToDoList'
 
 function App() {
 
   return (
     <>
       <section className='w-6/12 m-auto flex justify-center items-center pt-10'>
-        <TodoList />
+        <ToDoList />
       </section>
     </>
   )
--- src/components/todolist/TodoList.jsx
+++ src/components/todolist/TodoList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const ToDoList = () => {
     const [todos, setTodos] = useState([]);
